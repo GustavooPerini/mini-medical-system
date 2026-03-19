b25f152b3b8f5b71b2d7e5248e64b152c721ec26
GustavoPerini <gustavo.perini@edu.ufes.br>
2026-03-19 14:58:00 -0300

    Organizing the project structure

---
+++ backend/src/test/java/com/mini_medical_system/gustavo/perini/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini_medical_system.gustavo.perini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
